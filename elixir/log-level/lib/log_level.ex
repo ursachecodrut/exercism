9c467a2f7d9ace8351a0b12a4f171893059cf51f
Codrut Ursache <ursache.codrut71@gmail.com>
2023-02-10 17:09:44 +0200

    freelance. language-list. log-level

---
+++ elixir/log-level/lib/log_level.ex
@@ -0,0 +1,23 @@
+defmodule LogLevel do
+  def to_label(level, legacy?) do
+    cond do
+      level === 0 && !legacy? -> :trace
+      level === 1 -> :debug
+      level === 2 -> :info
+      level === 3 -> :warning
+      level === 4 -> :error
+      level === 5 && !legacy? -> :fatal
+      true -> :unknown
+    end
+  end
+
+  def alert_recipient(level, legacy?) do
+    label = to_label(level, legacy?)
+    cond do
+      label == :error || label == :fatal -> :ops
+      label == :unknown && legacy? -> :dev1
+      label == :unknown && !legacy? -> :dev2
+      true -> false
+    end
+  end
+end
